d402db21f5e41481ddeeba7daf2176a579d4aea4
ByteBlast <byteblast@gmail.com>
2015-01-03 05:20:02 +0000

    Now possible to input any number of poll options.

--- Poll/app/controllers/submitController.js
+++ Poll/app/controllers/submitController.js
@@ -3,12 +3,18 @@
     $scope.poll = {
         options: []
     };
+    $scope.poll.options.length = 3;
 
     $scope.submitPoll = function () {
+        $scope.poll.options = $scope.poll.options.filter(function () { return true; });
         $http.post('Poll', $scope.poll)
             .success(onPollSubmited);
     };
 
+    $scope.addOption = function () {
+        $scope.poll.options.length += 1;
+    };
+    
     function onPollSubmited(data, status, headers) {
         var submitedPollId = headers('Location');
         $location.path("/" + submitedPollId);
---
+++ Poll/app/directives/appendInputDirective.js
@@ -0,0 +1,10 @@
+﻿app.directive('appendInput', function() {
+
+    return {
+        restrict: 'A',
+        link: function (scope, element, attributes) {
+            element.html('<p>Hi</p>');
+            console.log("invoked");
+        }
+    };
+});
